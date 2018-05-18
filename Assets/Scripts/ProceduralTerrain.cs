f93682d5814038236fb59b17c3e550f7ca29cbc5
Bryan Maynard <bsgbryan@gmail.com>
2018-05-18 18:06:39 -0500

    Assign vector data to `vertices` and `triangles` data structures

--- Assets/Scripts/ProceduralTerrain.cs
+++ Assets/Scripts/ProceduralTerrain.cs
@@ -12,6 +12,11 @@ public class ProceduralTerrain : MonoBehaviour {
     int x_segments = TerrainSize / CellSize;
     int z_segments = TerrainSize / CellSize;
 
+    int vertex_count = 6 * x_segments * z_segments;
+
+    List<Vector3> vertices  = new List<Vector3>(new Vector3[vertex_count]);
+    List<int>     triangles = new List<int>    (new int    [vertex_count]);
+
     for (int x = 0; x < x_segments; x++) {
       for (int z = 0; z < z_segments; z++) {
         float height00 = GetHeight(x + 0f, z + 0f, x_segments, z_segments);
@@ -28,6 +33,28 @@ public class ProceduralTerrain : MonoBehaviour {
         var vertex01 = new Vector3((float) x0, height01 * (float) TerrainHeight, (float) z1);
         var vertex10 = new Vector3((float) x1, height10 * (float) TerrainHeight, (float) z0);
         var vertex11 = new Vector3((float) x1, height11 * (float) TerrainHeight, (float) z1);
+
+        int index0 = 6 * (x + z * x_segments);
+
+        int index1 = index0 + 1;
+        int index2 = index0 + 2;
+        int index3 = index0 + 3;
+        int index4 = index0 + 4;
+        int index5 = index0 + 5;
+
+        vertices[index0] = vertex00;
+        vertices[index1] = vertex01;
+        vertices[index2] = vertex11;
+        vertices[index3] = vertex00;
+        vertices[index4] = vertex11;
+        vertices[index5] = vertex10;
+
+        triangles[index0] = index0;
+        triangles[index1] = index1;
+        triangles[index2] = index2;
+        triangles[index3] = index3;
+        triangles[index4] = index4;
+        triangles[index5] = index5;
       }
     }
   }
