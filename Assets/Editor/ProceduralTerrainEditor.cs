4c5466035ceed75690f1f903e9282c6f9cca6e5f
Bryan Maynard <bsgbryan@gmail.com>
2018-05-18 18:01:02 -0500

    Created custom Editor for Procedural Terrain Generator

---
+++ Assets/Editor/ProceduralTerrainEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ProceduralTerrain))]
+[CanEditMultipleObjects]
+public class ProceduralTerrainEditor: Editor {
+  
+  public override void OnInspectorGUI() {
+    serializedObject.Update();
+
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("TerrainSize"));
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("CellSize"));
+
+    if (GUILayout.Button("Generate"))
+      (serializedObject.targetObject as ProceduralTerrain).GenerateTerrain();
+
+    serializedObject.ApplyModifiedProperties();
+  }
+}
