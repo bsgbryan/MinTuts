856ee18c91cb1c7d44ca4f2af7de51f533a91a8a
Bryan Maynard <bsgbryan@gmail.com>
2018-06-27 15:29:11 -0500

    Updated ProceduralTerrainEditor to reflect what it should be
    Something went wrong when I was trying to resolve merge conflicts.
    Somehow all the changes in the `Procedural-Terrain-2` branch were lost.
    This commit reimplements the changes for ProceduralTerrainEditor.

--- Assets/Editor/ProceduralTerrainEditor.cs
+++ Assets/Editor/ProceduralTerrainEditor.cs
@@ -8,13 +8,26 @@ public class ProceduralTerrainEditor: Editor {
   public override void OnInspectorGUI() {
     serializedObject.Update();
 
+    EditorGUI.BeginChangeCheck();
+
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("AutoUpdate"));
+
     EditorGUILayout.PropertyField(serializedObject.FindProperty("TerrainSize"));
     EditorGUILayout.PropertyField(serializedObject.FindProperty("TerrainHeight"));
     EditorGUILayout.PropertyField(serializedObject.FindProperty("CellSize"));
 
-    if (GUILayout.Button("Generate"))
-      (serializedObject.targetObject as ProceduralTerrain).GenerateTerrain();
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("Octaves"));
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("Scale"));
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("Persistance"));
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("Lacunarity"));
+
+    EditorGUILayout.PropertyField(serializedObject.FindProperty("UseFalloffMap"));
 
     serializedObject.ApplyModifiedProperties();
+
+    ProceduralTerrain procedural_terrain = serializedObject.targetObject as ProceduralTerrain;
+
+    if (GUILayout.Button("Generate") || (EditorGUI.EndChangeCheck() && procedural_terrain.AutoUpdate))
+      procedural_terrain.GenerateTerrain();
   }
 }
